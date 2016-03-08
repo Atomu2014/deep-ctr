525e8e95d57e1a6b4b55ce52ebaba668371956c2
Atomu2014 <kevinqu16@gmail.com>
2016-03-08 14:20:42 +0800

    feed batch with SparseTensor

--- python/LR_online.py
+++ python/LR_online.py
@@ -15,24 +15,59 @@ mask = np.where(cat_sizes < 10000)[0]
 offsets = [13 + sum(cat_sizes[mask[:i]]) for i in range(len(mask))]
 X_dim = 13 + np.sum(cat_sizes[mask])
 
-valid_dataset, valid_labels = load_svmlight('../data/day_0_test_x30_concat')
+# valid_dataset, valid_labels = load_svmlight('../data/day_0_test_x30_concat')
 
 file_list = ['../data/day_0_train_x30', '../data/day_0_train_x30']
 file_index = 0
 line_index = 0
+batch_size = 100
 
 
 def get_fxy(line):
     fields = line.split('\t')
+    y = fields[0]
     cats = fields[14:]
-    feats = range(13)
-    feats.extend([int(cats[mask[i]]) + offsets[i] for i in range(len(mask))])
+    f = range(13)
+    f.extend([int(cats[mask[i]]) + offsets[i] for i in range(len(mask))])
     x = [float(fields[i]) / max_vals[i - 1] for i in range(1, 14)]
     x.extend([1] * len(mask))
-    return fields[0], feats, x
+    return y, f, x
 
 
-def get_batch(batch_size):
+def get_batch_sparse_tensor(file_name, start_index, size, row_start=0):
+	labels = []
+	indices = []
+	values = []
+	for i in range(start_index, start_index + size):
+		line = linecache.getline(file_name, i)
+		if len(line.strip()):
+			y, f, x = get_fxy(line)
+			indices.extend([[row_start + len(labels), f[i]] for i in range(len(f))])
+			values.extend([x for x in values])
+			labels.append(y)
+		else:
+			break
+
+	return labels, indices, values
+
+
+def get_batch_xy():
+	global file_index, line_index, batch_size
+	labels, indices, values = get_batch_sparse_tensor(file_list[file_index], line_index, batch_size)
+	if len(labels) == batch_size:
+		line_index += batch_size
+		return labels, indices, batch_size
+
+	file_index = (file_index + 1) % len(file_list)
+	line_index = batch_size - len(labels)
+	l, i, v = get_batch_sparse_tensor(file_list[file_index], 0, batch_size - len(labels))
+	labels.extend(l)
+	indices.extend(i)
+	values.extend(v)
+	return labels, indices, values
+
+
+def get_batch_csr(batch_size):
     global line_index, file_index
     labels = []
     rows = []
@@ -82,15 +117,17 @@ def auc(predictions, labels):
 _learning_rate = 0.001
 _l2_param = 0.001
 _keep_prob = 0.5
-_batch_size = 100
+
 
 print 'batch_size: %d, learning_rate: %f, l2_param: %f, keep_prob: %f' % (
-    _batch_size, _learning_rate, _l2_param, _keep_prob)
+    batch_size, _learning_rate, _l2_param, _keep_prob)
 
 graph = tf.Graph()
 with graph.as_default():
-    tf_train_dataset = tf.placeholder(tf.float32, shape=(_batch_size, X_dim))
-    tf_train_labels = tf.placeholder(tf.float32, shape=(_batch_size, 1))
+	tf_train_indices = tf.placeholder(tf.float32, shape=(batch_size * (13 + len(mask)), 2))
+	tf_train_values = tf.placeholder(tf.float32, shape=(batch_size * (13 + len(mask))))
+    tf_train_dataset = tf.SparseTensor(tf_train_indices, tf_train_values, [batch_size * X_dim])
+    tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, 1))
     tf_valid_dataset = tf.constant(tf.SparseTensor())
     # tf_test_dataset = tf.constant(test_dataset)
 
@@ -103,7 +140,7 @@ with graph.as_default():
     optimizer = tf.train.GradientDescentOptimizer(_learning_rate).minimize(loss)
 
     train_pred = tf.sigmoid(logits)
-    valid_pred = tf.sigmoid(tf.matmul(tf_valid_dataset, weights, a_is_sparse=True) + bias)
+    # valid_pred = tf.sigmoid(tf.matmul(tf_valid_dataset, weights, a_is_sparse=True) + bias)
     # test_pred = tf.sigmoid(tf.matmul(tf_test_dataset, weights, a_is_sparse=True) + bias)
 
 with tf.Session(graph=graph) as session:
@@ -113,11 +150,13 @@ with tf.Session(graph=graph) as session:
     step = 0
     while True:
         step += 1
-        batch_labels, batch_data = get_batch(_batch_size)
+        batch_labels, batch_indices, batch_values = get_batch_xy(batch_size)
 
         feed_dict = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
         _, l, pred = session.run([optimizer, loss, train_pred], feed_dict=feed_dict)
         if step % 100 == 0:
             print 'loss as step %d: %f' % (step, l)
             print 'train-auc: %f%%\teval-auc: %f%%' % (
-                roc_auc_score(batch_labels, pred), roc_auc_score(valid_labels, valid_pred))
+                roc_auc_score(batch_labels, pred), 
+                #roc_auc_score(valid_labels, valid_pred))
+				0)
