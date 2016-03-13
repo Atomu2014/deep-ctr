67d899db6e5da02ce23b2fa2ea3e095d5cc04c4e
Atomu2014 <kevinqu16@gmail.com>
2016-03-13 16:43:41 +0800

    nds 10% shuffle fm remove duplicate dense matrix representation

--- python/baseline_online.py
+++ python/baseline_online.py
@@ -6,9 +6,10 @@ max_vals = [65535, 8000, 2330, 746810, 8000, 57199, 5277, 225635, 3565, 14, 310,
 cat_sizes = np.array(
     [18576837, 29427, 15127, 7295, 19901, 3, 6465, 1310, 61, 11700067, 622921, 219556, 10, 2209, 9779, 71, 4, 963, 14,
      22022124, 4384510, 15960286, 290588, 10829, 95, 34])
-mask = np.where(cat_sizes < 1000)[0]
+mask = np.where(cat_sizes < 100000)[0]
 offsets = [13 + sum(cat_sizes[mask[:i]]) for i in range(len(mask))]
 X_dim = 13 + np.sum(cat_sizes[mask])
+X_feas = 13 + len(mask)
 
 print len(mask), np.sum(cat_sizes[mask])
 
@@ -18,13 +19,14 @@ file_index = 0
 line_index = 0
 
 batch_size = 1
+buffer_size = 10000
 epoch = 1000
 
 _learning_rate = 0.001
 _alpha = 1
 _lambda = 0.001
 _keep_prob = 0.5
-_stddev = 0.01
+_stddev = 0.1
 _rank = 20
 
 
@@ -46,14 +48,14 @@ def get_batch_sparse_tensor(file_name, start_index, size, row_start=0):
     labels = []
     cols = []
     values = []
-    indices = []
+    rows = []
     row_num = row_start
     for i in range(start_index, start_index + size):
         try:
             line = next(fin)
             if len(line.strip()):
                 y, f, x = get_fxy(line)
-                indices.extend([[row_num, c] for c in f])
+                rows.extend([row_num] * len(f))
                 cols.extend(f)
                 values.extend(x)
                 labels.append(y)
@@ -65,24 +67,24 @@ def get_batch_sparse_tensor(file_name, start_index, size, row_start=0):
             fin = None
             break
 
-    return labels, indices, cols, values
+    return labels, rows, cols, values
 
 
 def get_batch_xy(size):
     global file_index, line_index
-    labels, indices, cols, values = get_batch_sparse_tensor(file_list[file_index], line_index, size)
+    labels, rows, cols, values = get_batch_sparse_tensor(file_list[file_index], line_index, size)
     if len(labels) == size:
         line_index += size
-        return np.array(labels), np.array(indices), np.array(cols), np.array(values)
+        return np.array(labels), np.array(rows), np.array(cols), np.array(values)
 
     file_index = (file_index + 1) % len(file_list)
     line_index = size - len(labels)
-    l, i, c, v = get_batch_sparse_tensor(file_list[file_index], 0, size - len(labels))
+    l, r, c, v = get_batch_sparse_tensor(file_list[file_index], 0, size - len(labels))
     labels.extend(l)
-    indices.extend(i)
+    rows.extend(r)
     cols.extend(c)
     values.extend(v)
-    return np.array(labels), np.array(indices), np.array(cols), np.array(values)
+    return np.array(labels), np.array(rows), np.array(cols), np.array(values)
 
 
 print 'batch: %d, epoch: %d, lr: %.4f, lambda: %.4f, stddev: %.4f, keep_prob: %.4f, alpha: %.4f' % (
@@ -90,13 +92,11 @@ print 'batch: %d, epoch: %d, lr: %.4f, lambda: %.4f, stddev: %.4f, keep_prob: %.
 
 with open('../data/day_0_test_x30', 'r') as valid_fin:
     valid_labels = []
-    valid_inds = []
     valid_cols = []
     valid_vals = []
     valid_num_row = 0
     for line in valid_fin:
         y, f, x = get_fxy(line)
-        valid_inds.extend([[valid_num_row, c] for c in f])
         valid_cols.extend(f)
         valid_vals.extend(x)
         valid_labels.append(y)
@@ -104,62 +104,21 @@ with open('../data/day_0_test_x30', 'r') as valid_fin:
         if valid_num_row == 10000:
             break
 
-valid_rows = []
+valid_inds = []
 for i in range(valid_num_row):
-    for j in range(13 + len(mask)):
-        valid_rows.append([i, j])
+    for j in range(X_feas):
+        valid_inds.append([i, j])
+
+valid_sq_vals = np.array(valid_vals) ** 2
 
 sp_indices = []
 for i in range(batch_size):
-    for j in range(13 + len(mask)):
+    for j in range(X_feas):
         sp_indices.append([i, j])
 
 
-def lr_bgd():
-    graph = tf.Graph()
-    with graph.as_default():
-        tf_sp_id_vals = tf.placeholder(tf.int64, shape=[batch_size * (13 + len(mask))])
-        tf_sp_weight_vals = tf.placeholder(tf.float32, shape=[batch_size * (13 + len(mask))])
-        tf_sp_ids = tf.SparseTensor(sp_indices, tf_sp_id_vals, shape=[batch_size, 13 + len(mask)])
-        tf_sp_weights = tf.SparseTensor(sp_indices, tf_sp_weight_vals, shape=[batch_size, 13 + len(mask)])
-        tf_train_labels = tf.placeholder(tf.float32, shape=[batch_size])
-        tf_valid_ids = tf.SparseTensor(valid_rows, valid_cols, shape=[valid_num_row, 13 + len(mask)])
-        tf_valid_weights = tf.SparseTensor(valid_rows, valid_vals, shape=[valid_num_row, 13 + len(mask)])
-
-        weights = tf.Variable(tf.truncated_normal([X_dim, 1], stddev=_stddev))
-        bias = tf.Variable(0.0)
-
-        logits = tf.nn.embedding_lookup_sparse(weights, tf_sp_ids, tf_sp_weights, combiner='sum') + bias
-        loss = tf.reduce_mean(
-            tf.nn.sigmoid_cross_entropy_with_logits(logits, tf_train_labels)) + _lambda * tf.nn.l2_loss(
-            weights)
-
-        optimizer = tf.train.GradientDescentOptimizer(_learning_rate).minimize(loss)
-
-        train_pred = tf.sigmoid(logits)
-        valid_pred = tf.sigmoid(
-            tf.nn.embedding_lookup_sparse(weights, tf_valid_ids, tf_valid_weights, combiner='sum') + bias)
-
-    with tf.Session(graph=graph) as session:
-        tf.initialize_all_variables().run()
-        print 'initialized'
-
-        step = 0
-        while True:
-            step += 1
-            batch_labels, batch_cols, batch_values = get_batch_xy(batch_size)
-
-            feed_dict = {tf_sp_id_vals: batch_cols, tf_sp_weight_vals: batch_values,
-                         tf_train_labels: batch_labels}
-            _, l, pred = session.run([optimizer, loss, train_pred], feed_dict=feed_dict)
-            if step % epoch == 0:
-                print 'loss as step %d: %f' % (step, l)
-                try:
-                    # batch_auc = roc_auc_score(batch_labels, pred)
-                    valid_auc = roc_auc_score(valid_labels, valid_pred.eval())
-                    print 'eval-auc: %.4f' % valid_auc
-                except ValueError as e:
-                    print 'None'
+def get_sparse_indices(rows, cols):
+    return np.array([[rows[i], cols[i]] for i in range(len(rows))])
 
 
 def lr_sgd():
@@ -167,13 +126,13 @@ def lr_sgd():
 
     graph = tf.Graph()
     with graph.as_default():
-        tf_sp_id_vals = tf.placeholder(tf.int64, shape=[13 + len(mask)])
-        tf_sp_weight_vals = tf.placeholder(tf.float32, shape=[13 + len(mask)])
-        tf_sp_ids = tf.SparseTensor(sp_indices, tf_sp_id_vals, shape=[batch_size, 13 + len(mask)])
-        tf_sp_weights = tf.SparseTensor(sp_indices, tf_sp_weight_vals, shape=[batch_size, 13 + len(mask)])
-        tf_train_label = tf.placeholder(tf.float32, shape=[batch_size])
-        tf_valid_ids = tf.SparseTensor(valid_rows, valid_cols, shape=[batch_size, 13 + len(mask)])
-        tf_valid_weights = tf.SparseTensor(valid_rows, valid_vals, shape=[batch_size, 13 + len(mask)])
+        tf_sp_id_vals = tf.placeholder(tf.int64, shape=[X_feas])
+        tf_sp_weight_vals = tf.placeholder(tf.float32, shape=[X_feas])
+        tf_sp_ids = tf.SparseTensor(sp_indices, tf_sp_id_vals, shape=[1, X_feas])
+        tf_sp_weights = tf.SparseTensor(sp_indices, tf_sp_weight_vals, shape=[1, X_feas])
+        tf_train_label = tf.placeholder(tf.float32)
+        tf_valid_ids = tf.SparseTensor(valid_inds, valid_cols, shape=[1, X_feas])
+        tf_valid_weights = tf.SparseTensor(valid_inds, valid_vals, shape=[1, X_feas])
 
         weights = tf.Variable(tf.truncated_normal([X_dim, 1], stddev=_stddev))
         bias = tf.Variable(0.0)
@@ -191,22 +150,22 @@ def lr_sgd():
     with tf.Session(graph=graph) as session:
         tf.initialize_all_variables().run()
         print 'initialized'
-
         step = 0
         while True:
-            step += 1
-            _label, _cols, _values = get_batch_xy(batch_size)
-
-            feed_dict = {tf_sp_id_vals: _cols, tf_sp_weight_vals: _values, tf_train_label: _label}
-            _, l, pred = session.run([optimizer, loss, train_pred], feed_dict=feed_dict)
-            if step % epoch == 0:
-                print 'loss as step %d: %f' % (step, l)
-                try:
-                    # batch_auc = roc_auc_score(_label, pred)
-                    valid_auc = roc_auc_score(valid_labels, valid_pred.eval())
-                    print 'eval-auc: %.4f' % valid_auc
-                except ValueError as e:
-                    print 'None'
+            label, _, cols, values = get_batch_xy(buffer_size)
+            for i in range(label.shape[0]):
+                step += 1
+                feed_dict = {tf_sp_id_vals: cols[i * X_feas: (i + 1) * X_feas],
+                             tf_sp_weight_vals: values[i * X_feas: (i + 1) * X_feas],
+                             tf_train_label: label[i]}
+                _, l, pred = session.run([optimizer, loss, train_pred], feed_dict=feed_dict)
+                if step % epoch == 0:
+                    print 'loss as step %d: %f' % (step, l)
+                    try:
+                        valid_auc = roc_auc_score(valid_labels, valid_pred.eval())
+                        print 'eval-auc: %.4f' % valid_auc
+                    except ValueError as e:
+                        print 'None'
 
 
 def fm_sgd():
@@ -215,83 +174,64 @@ def fm_sgd():
 
     graph = tf.Graph()
 
-    def factorization(sp_ids, sp_weights, x):
+    def factorization(sp_ids, sp_weights, sp_sq_weights):
         yhat = tf.nn.embedding_lookup_sparse(W, sp_ids, sp_weights, combiner='sum') + b
-        # wb_shape = tf.shape(yhat)
         vx = tf.nn.embedding_lookup_sparse(V, sp_ids, sp_weights, combiner='sum')
-        # vx = tf.matmul(x, V)
-        # vx_shape = tf.shape(vx)
-        # vxvx = tf.matmul(vx, vx, transpose_b=True)
-        # vxvx_shape = tf.shape(vxvx)
-        # vxvxs = tf.reduce_sum(vxvx, 1)
-        # vxvxs_shape = tf.shape(vxvxs)
-        # x2 = tf.square(x)
-        # x2_shape = tf.shape(x2)
-        # V2 = tf.square(V)
-        # V2_shape = tf.shape(V2)
-        # x2v2 = tf.matmul(x2, V2)
-        # x2v2_shape = tf.shape(x2v2)
-        # x2v2s = tf.reduce_sum(x2v2, 1)
-        # x2v2s_shape = tf.shape(x2v2s)
+        V2 = tf.square(V)
+        V2x2 = tf.nn.embedding_lookup_sparse(V2, sp_ids, sp_sq_weights, combiner='sum')
         yhat += 0.5 * tf.reshape(
-            tf.reduce_sum(tf.matmul(vx, vx, transpose_b=True), 1) - tf.reduce_sum(tf.matmul(tf.square(x), tf.square(V)),
-                                                                                  1), shape=[-1, 1])
+            tf.reduce_sum(tf.matmul(vx, vx, transpose_b=True), 1) - tf.reduce_sum(
+                V2x2,
+                1), shape=[-1, 1])
         return yhat
-        # , wb_shape, vx_shape, vxvx_shape, vxvxs_shape, x2_shape, V2_shape, x2v2_shape, x2v2s_shape
 
     with graph.as_default():
-        tf_sp_id_vals = tf.placeholder(tf.int64, shape=[13 + len(mask)])
-        tf_sp_weight_vals = tf.placeholder(tf.float32, shape=[13 + len(mask)])
-        tf_sp_ids = tf.SparseTensor(sp_indices, tf_sp_id_vals, shape=[batch_size, 13 + len(mask)])
-        tf_sp_weights = tf.SparseTensor(sp_indices, tf_sp_weight_vals, shape=[batch_size, 13 + len(mask)])
-        tf_train_label = tf.placeholder(tf.float32, shape=[1])
-        tf_train_indices = tf.placeholder(tf.int64, shape=[batch_size * (13 + len(mask)), 2])
-        tf_train = tf.sparse_to_dense(tf_train_indices, [batch_size, X_dim], tf_sp_weight_vals)
-        tf_valid_ids = tf.SparseTensor(valid_rows, valid_cols, shape=[valid_num_row, 13 + len(mask)])
-        tf_valid_weights = tf.SparseTensor(valid_rows, valid_vals, shape=[valid_num_row, 13 + len(mask)])
-        tf_valid = tf.sparse_to_dense(valid_inds, [valid_num_row, X_dim], valid_vals)
+        tf_sp_id_vals = tf.placeholder(tf.int64, shape=[X_feas])
+        tf_sp_weight_vals = tf.placeholder(tf.float32, shape=[X_feas])
+        tf_sp_ids = tf.SparseTensor(sp_indices, tf_sp_id_vals, shape=[1, X_feas])
+        tf_sp_weights = tf.SparseTensor(sp_indices, tf_sp_weight_vals, shape=[1, X_feas])
+        tf_train_label = tf.placeholder(tf.float32)
+        tf_sp_sq_weight_vals = tf.placeholder(tf.float32, shape=[X_feas])
+        tf_sp_sq_weights = tf.SparseTensor(sp_indices, tf_sp_sq_weight_vals, shape=[1, X_feas])
+        tf_valid_ids = tf.SparseTensor(valid_inds, valid_cols, shape=[valid_num_row, X_feas])
+        tf_valid_weights = tf.SparseTensor(valid_inds, valid_vals, shape=[valid_num_row, X_feas])
+        tf_valid_sq_weights = tf.SparseTensor(valid_inds, valid_sq_vals, shape=[valid_num_row, X_feas])
 
         W = tf.Variable(tf.truncated_normal([X_dim, 1], stddev=_stddev))
         b = tf.Variable(0.0)
         V = tf.Variable(tf.truncated_normal([X_dim, _rank], stddev=_stddev))
 
-        logit = factorization(tf_sp_ids, tf_sp_weights, tf_train)
+        logit = factorization(tf_sp_ids, tf_sp_weights, tf_sp_sq_weights)
         loss = tf.nn.sigmoid_cross_entropy_with_logits(logit, tf_train_label) + _lambda * (
             tf.nn.l2_loss(W) + tf.nn.l2_loss(V) + tf.nn.l2_loss(b))
         optimizer = tf.train.GradientDescentOptimizer(_learning_rate).minimize(loss)
         train_pred = tf.sigmoid(logit)
-        valid_logits = factorization(tf_valid_ids, tf_valid_weights, tf_valid)
+        valid_logits = factorization(tf_valid_ids, tf_valid_weights, tf_valid_sq_weights)
         valid_pred = tf.sigmoid(valid_logits)
-        # vp_shape = tf.shape(valid_pred)
 
     with tf.Session(graph=graph) as session:
         tf.initialize_all_variables().run()
         print 'initialized'
-
         step = 0
         while True:
-            step += 1
-            _label, _ind, _cols, _values = get_batch_xy(1)
+            label, rows, cols, values = get_batch_xy(buffer_size)
+            for i in range(label.shape[0]):
+                step += 1
+                _label = label[i]
+                _cols = cols[i * X_feas: (i + 1) * X_feas]
+                _vals = values[i * X_feas: (i + 1) * X_feas]
+                _sqvals = _vals ** 2
 
-            feed_dict = {tf_sp_id_vals: _cols, tf_sp_weight_vals: _values, tf_train_label: _label,
-                         tf_train_indices: _ind}
-            _, l, pred, lg = session.run([optimizer, loss, train_pred, logit], feed_dict=feed_dict)
-            if step % epoch == 0:
-                print 'loss as step %d: %f' % (step, l)
-                try:
-                    # print 'wb', wb_shape.eval()
-                    # print 'vx', vx_shape.eval()
-                    # print 'vxvx', vxvx_shape.eval()
-                    # print 'vxvxs', vxvxs_shape.eval()
-                    # print 'x2', x2_shape.eval()
-                    # print 'V2', V2_shape.eval()
-                    # print 'x2v2', x2v2_shape.eval()
-                    # print 'x2v2s', x2v2s_shape.eval()
-                    # print 'vp', vp_shape.eval()
-                    valid_auc = roc_auc_score(valid_labels, valid_pred.eval())
-                    print 'eval-auc: %.4f' % valid_auc
-                except ValueError as e:
-                    print 'None'
+                feed_dict = {tf_sp_id_vals: _cols, tf_sp_weight_vals: _vals, tf_sp_sq_weight_vals: _sqvals,
+                             tf_train_label: _label}
+                _, l, pred = session.run([optimizer, loss, train_pred], feed_dict=feed_dict)
+                if step % epoch == 0:
+                    print 'loss as step %d: %f' % (step, l)
+                    try:
+                        valid_auc = roc_auc_score(valid_labels, valid_pred.eval())
+                        print 'eval-auc: %.4f' % valid_auc
+                    except ValueError as e:
+                        print 'None'
 
 
 if __name__ == '__main__':
--- python/clean.py
+++ python/clean.py
@@ -137,19 +137,90 @@ def concat(file_name, mask):
                 fout.write(buffer)
 
 
-if __name__ == '__main__':
-    # train_path = '../data/day_0_train_x30'
-    # test_path = '../data/day_0_test_x30'
-    # sample(train_path, test_path, alpha=0.06)
-    # pos_neg_ratio(train_path)
-    # pos_neg_ratio(test_path)
-    # concat(train_path, np.where(cat_sizes < 10000)[0])
-    # concat(test_path, np.where(cat_sizes < 10000)[0])
-    max_vals = [0] * 13
-    sets = [{} for i in range(26)]
-    long_tails = [set() for i in range(26)]
+def nds(fin_path, fout_path, w):
+    cnt = 0
 
-    for i in range(7, 15):
-        print 'processing day_' + str(i)
-        stat('../data/day_' + str(i), sets, long_tails, max_vals)
-        pickle.dump({'max_vals': max_vals, 'sets': sets, 'long_tails': long_tails}, open('../data/stat.pickle', 'wb'))
+    def nds_sample(lines):
+        _buf_str = ''
+        _buf_size = 0
+        for line in lines:
+            if int(line.strip().split()[0]) == 0:
+                if random.random() < w:
+                    _buf_size += 1
+                    _buf_str += line
+            else:
+                _buf_size += 1
+                _buf_str += line
+        return _buf_str, _buf_size
+
+    with open(fin_path, 'rb') as fin:
+        with open(fout_path, 'ab') as fout:
+            start_time = time.time()
+            buf = []
+            buf_str = ''
+            buf_size = 0
+            for line in fin:
+                buf.append(line)
+                cnt += 1
+
+                if cnt % 10000 == 0:
+                    s, n = nds_sample(buf)
+                    buf_str += s
+                    buf_size += n
+                    buf = []
+                    if buf_size > 10000:
+                        fout.write(buf_str)
+                        buf_str = ''
+                        buf_size = 0
+
+                    if cnt % 10000000 == 0:
+                        end_time = time.time()
+                        print cnt, end_time - start_time
+                        start_time = end_time
+
+            if len(buf) > 0:
+                s, n = nds_sample(buf)
+                buf_str += s
+                buf_size += n
+            if buf_size > 0:
+                fout.write(buf_str)
+
+
+def seg_file(fin_path):
+    def output(buf, fout_name):
+        with open(fout_name, 'wb') as fout:
+            buf_str = ''
+            buf_size = 0
+            for line in buf:
+                buf_str += line
+                buf_size += 1
+
+                if buf_size % 10000 == 0:
+                    fout.write(buf_str)
+                    buf_str = ''
+                    buf_size = 0
+            if buf_size:
+                fout.write(buf_str)
+
+    with open(fin_path, 'rb') as fin:
+        cnt = 0
+        buf = []
+        for line in fin:
+            cnt += 1
+            buf.append(line)
+
+            if cnt % 4000000 == 0:
+                np.random.shuffle(buf)
+                output(buf, fin_path + '.%d' % (cnt / 4000000))
+                buf = []
+        if len(buf):
+            output(buf, fin_path + '.%d' % (cnt / 4000000 + 1))
+
+if __name__ == '__main__':
+    # w = 0.1
+    # for i in range(7, 14):
+    #     print 'processing day_' + str(i)
+    #     nds('../data/day_' + str(i), '../data/nds.10', w)
+    # nds('../data/day_14', '../data/test.nds.10', w)
+    seg_file('../data/nds.10')
+    seg_file('../data/test.nds.10')
