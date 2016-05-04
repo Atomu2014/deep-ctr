ipinyou test; fix wrong formula

python/FM.py
@@ -36,6 +36,17 @@ class FM:
                 sp_eval_wts2 = tf.SparseTensor(sp_eval_inds, eval_wts2, shape=[eval_size, X_feas])
                 _lambda = _reg_argv[0]
 
+                # # y = w * x + b
+                # self.yhat = tf.nn.embedding_lookup_sparse(self.W, sp_ids, sp_wts, combiner='sum') + self.b
+                # # vx = v * x
+                # self._Vx = tf.nn.embedding_lookup_sparse(self.V, sp_ids, sp_wts, combiner='sum')
+                # # v2x2 = v ^ 2 * x ^ 2
+                # self._V2x2 = tf.nn.embedding_lookup_sparse(tf.square(self.V), sp_ids, sp_wts2, combiner='sum')
+                # # y = w * x + b + 1/2 * (sum(vx * vx) - sum(v2x2))
+                # self.s_vx = tf.reduce_sum(tf.square(self._Vx), 1)
+                # self.s_v2x2 = tf.reduce_sum(self._V2x2, 1)
+                # self.yhat += 0.5 * tf.reshape(self.s_vx - self.s_v2x2, shape=[-1, 1])
+
                 logits = self.factorization(sp_ids, sp_wts, sp_wts2)
                 self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits, self.lbl_hldr)) + _lambda * (
                     tf.nn.l2_loss(self.W) + tf.nn.l2_loss(self.V) + tf.nn.l2_loss(self.b))
@@ -49,11 +60,14 @@ class FM:
                 self.test_preds = tf.sigmoid(logits)
 
     def factorization(self, sp_ids, sp_weights, sp_weights2):
+        # y = w * x + b
         yhat = tf.nn.embedding_lookup_sparse(self.W, sp_ids, sp_weights, combiner='sum') + self.b
+        # vx = v * x
         _Vx = tf.nn.embedding_lookup_sparse(self.V, sp_ids, sp_weights, combiner='sum')
+        # v2x2 = v ^ 2 * x ^ 2
         _V2x2 = tf.nn.embedding_lookup_sparse(tf.square(self.V), sp_ids, sp_weights2, combiner='sum')
-        yhat += 0.5 * tf.reshape(tf.reduce_sum(tf.matmul(_Vx, _Vx, transpose_b=True), 1) - tf.reduce_sum(_V2x2, 1),
-                                 shape=[-1, 1])
+        # y = w * x + b + 1/2 * (sum(vx * vx) - sum(v2x2))
+        yhat += 0.5 * tf.reshape(tf.reduce_sum(tf.square(_Vx), 1) - tf.reduce_sum(_V2x2, 1), shape=[-1, 1])
         return yhat
 
     def dump(self, model_path):

python/baseline.py
@@ -12,8 +12,8 @@ from LR import LR
 gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
 sess_config = tf.ConfigProto(gpu_options=gpu_options)
 
-train_path = '../data/nds.2.5.shuf.ind.100'
-eval_path = '../data/test.unif.2.5.shuf.ind.100'
+train_path = '../data/nds.2.5.shuf.ind.10'
+eval_path = '../data/test.unif.2.5.shuf.ind.10'
 test_path = eval_path
 nds_rate = 0.025
 re_calibration = 'nds' not in eval_path
@@ -24,14 +24,14 @@ data_set = None
 # cat_sizes = np.array([631838, 25296, 15533, 7108, 19272, 3, 6753, 1302, 54, 527814, 147690, 110673,
 #                       10, 2201, 9022, 62, 4, 945, 14, 639541, 357566, 600327, 118441, 10216, 77, 33])
 # nds = 0.025, ind = 10
-# max_vals = np.array([65535, 8000, 715, 376613, 7995, 1430, 2191, 25462, 5337, 8, 221, 3023897, 8072])
-# cat_sizes = np.array([284414, 22289, 15249, 7022, 18956, 3, 6730, 1286, 50, 253234, 93011, 81371,
-#                       10, 2187, 8396, 61, 4, 932, 14, 286828, 190408, 274540, 79781, 9690, 70, 33])
+max_vals = np.array([65535, 8000, 715, 376613, 7995, 1430, 2191, 25462, 5337, 8, 221, 3023897, 8072])
+cat_sizes = np.array([284414, 22289, 15249, 7022, 18956, 3, 6730, 1286, 50, 253234, 93011, 81371,
+                      10, 2187, 8396, 61, 4, 932, 14, 286828, 190408, 274540, 79781, 9690, 70, 33])
 # nds = 0.025, ind = 100
-max_vals = [65535, 8000, 715, 376613, 7995, 1430, 2191, 25462, 5337, 8, 221, 3023897, 8072]
-cat_sizes = np.array(
-    [31088, 13592, 13296, 6690, 17915, 3, 6497, 1217, 35, 34870, 24267, 32179, 10, 1959, 6076, 55, 4, 900, 14,
-     30404, 34948, 32786, 22420, 8392, 50, 33])
+# max_vals = [65535, 8000, 715, 376613, 7995, 1430, 2191, 25462, 5337, 8, 221, 3023897, 8072]
+# cat_sizes = np.array(
+#     [31088, 13592, 13296, 6690, 17915, 3, 6497, 1217, 35, 34870, 24267, 32179, 10, 1959, 6076, 55, 4, 900, 14,
+#      30404, 34948, 32786, 22420, 8392, 50, 33])
 
 cat_sizes += 1
 offsets = [13 + sum(cat_sizes[:i]) for i in range(len(cat_sizes))]
@@ -44,7 +44,7 @@ print 'cat_sizes (including \'other\'):', cat_sizes
 
 mode = 'train'
 # 'LR', 'FMxxx', 'FNN'
-algo = 'LR'
+algo = 'FM10'
 tag = (time.strftime('%c') + ' ' + algo).replace(' ', '_')
 log_path = '../log/%s' % tag
 model_path = '../model/%s.pickle' % tag
@@ -72,15 +72,15 @@ if 'LR' in algo:
     _reg_argv = [1e-4]
 elif 'FM' in algo:
     rank = int(algo[2:])
-    batch_size = 1
+    batch_size = 10
     test_batch_size = 100
     epoch = 100
     _rch_argv = [X_dim, X_feas, rank]
     _min_val = -1e-2
     _init_argv = ['uniform', _min_val, -1 * _min_val, seeds_pool[2:4],
                   '../model/Mon_Apr_11_09_42_50_2016_FM10.pickle_1335000']
-    _ptmzr_argv = ['ftrl', 2e-3]
-    _reg_argv = [1e-2]
+    _ptmzr_argv = ['ftrl', 1e-4]
+    _reg_argv = [1e-3]
 elif 'FNN' in algo:
     rank = int(algo[3:])
     batch_size = 1